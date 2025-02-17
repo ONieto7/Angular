a31be308995a035f482077bb176b56b13a1f81cf
ONieto7 <oscarniro@gmail.com>
2025-02-17 19:14:26 +0100

    Creacion proyecto neuvo

--- TiendaOnline/src/app/listado-productos/listado-productos.component.ts
+++ TiendaOnline/src/app/listado-productos/listado-productos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from '../producto/producto.component';
+import { Producto } from '../producto/producto.model';
 
 @Component({
   selector: 'app-listado-productos',
@@ -9,5 +10,10 @@ import { ProductoComponent } from '../producto/producto.component';
   styleUrl: './listado-productos.component.css'
 })
 export class ListadoProductosComponent {
-
+  productos: Producto[] = [
+    new Producto('Pantalon', 130.0),
+    new Producto('Camisa', 80.0),
+    new Producto('Zapatilla', 50.0), 
+  ];
+  
 }
