e73a3ea5bba30714aa96e13b43be613b5267ba6f
ONieto7 <oscarniro@gmail.com>
2025-02-21 13:30:53 +0100

    Añadir input

--- TiendaOnline/src/app/listado-productos/listado-productos.component.ts
+++ TiendaOnline/src/app/listado-productos/listado-productos.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from '../producto/producto.component';
 import { Producto } from '../producto/producto.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listado-productos',
   standalone: true,
-  imports: [ProductoComponent],
+  imports: [ProductoComponent, FormsModule],
   templateUrl: './listado-productos.component.html',
   styleUrl: './listado-productos.component.css'
 })
@@ -16,4 +17,20 @@ export class ListadoProductosComponent {
     new Producto('Zapatilla', 50.0), 
   ];
   
+  descripcionInput: string = '';
+  precionInput: number | null = null;
+
+  agregarProducto() {
+    //Validar que sean valores correcto
+    if(this.descripcionInput === '' || this.precionInput === null || this.precionInput <= 0){
+      console.log('Datos incorrectos');
+    }
+
+    const producto = new Producto(this.descripcionInput, this.precionInput!);
+    this.productos.push(producto);
+
+    // Limpiamos los campos del formulario
+    this.descripcionInput = '';
+    this.precionInput = null;
+  }
 }
